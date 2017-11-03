9842b814515d61990dbcf2537a9fd3736ddaaf5b
Joseph Moloughney <jmoloughney25@gmail.com>
2017-11-03 13:40:04 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,11 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+game_hash = {
+  :home => {:team_name => "Brooklyn Nets",
+            :colors => ["Black","White"],
+            :players => {"Alan_Anderson" => {:stats => {:number => "0"}}}
+  },
+  :away => {:team_name => "Charlotte Hornets",
+            :colors => ["Turquoise","Purple"],
+            :players => {}
+}}
+end
